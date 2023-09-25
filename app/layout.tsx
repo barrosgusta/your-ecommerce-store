c243406591255b32a140562ce3ef0db455b4d53c
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-09-24 21:46:51 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,22 @@
 import Footer from '@/components/footer'
+import Navbar from '@/components/navbar'
+
 import './globals.css'
 import type { Metadata } from 'next'
 import { Urbanist } from 'next/font/google'
+import ModalProvider from '@/providers/modal-provider'
+import ToastProvider from '@/providers/toast-provider'
+import { ThemeProvider } from '@/providers/theme-provider'
 
 const font = Urbanist({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Your Store',
-  description: 'Your Store',
+  title: 'Sua Loja',
+  description: 'Sua Loja',
 }
+
+// Opt out of caching for all data requests in the route segment
+export const dynamic = 'force-dynamic'
 
 export default function RootLayout({
   children,
@@ -18,8 +26,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        {children}
-        <Footer />
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+        >
+          <ModalProvider />
+          <ToastProvider /> 
+          <Navbar />
+          {children}
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   )
